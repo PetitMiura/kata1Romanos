2064604883bd5a2e2badd8988e5511baae3fdf52
Alejandro <petitmiura@gmail.com>
2023-05-22 23:05:46 +0200

    Kata del lunes 22 mayo

--- romannumbers.py
+++ romannumbers.py
@@ -1,23 +1,94 @@
-'''
-simbolos = {
+
+unidades = {
     1: "I",
     5: "V",
     10: "X"
 }
 
-def entero_a_romano(n_int):
-    if n_int in simbolos:
-        return simbolos[n_int]
+decenas = {
+    1: "X",
+    5: "L",
+    10: "C"
+}
 
-    if n_int < 4:
-        return n_int * simbolos[1]
-    elif n_int == 4:
-        return simbolos[1] + simbolos[5]
-    elif n_int >5 and n_int < 9:
-        multiplicador = n_int - 5
-        return simbolos[5] + multiplicador * simbolos[1]
+centenas = {
+    1: "C",
+    5: "D",
+    10: "M"
+}
+
+millares = {
+
+    1: "M"
+}
+
+
+class RomanNumberError(Exception):
+    pass
+
+def listar_numero(num):
+    n_mil = num // 1000 * 1000
+    n_cent = (num % 1000) // 100 * 100
+    n_dec = (num % 100) // 10 * 10
+    n_uni = (num % 10)
+
+    return n_mil, n_cent, n_dec, n_uni 
+
+def sacar_clave(num):
+
+    if num >= 1000:
+        clave = millares
+        num = num // 1000
+
+    elif num >= 100:
+        clave = centenas
+        num //= 100
+
+    elif num >= 10:
+        clave = decenas
+        num = num // 10
     else:
-        return simbolos[1] + simbolos[10]
+        clave = unidades
+    return clave, num
+
+def logica_aplastante(digito, clau):
+    res = ""
+
+    if digito < 4:
+        res += digito * clau[1]
+
+    elif digito == 4:
+        res += clau[1] + clau[5]
+
+    elif digito < 9:
+        num_palitos = digito - 5
+        res += clau[5] + num_palitos * clau[1]
+
+    else:
+        res += clau[1] + clau[10]
+
+    return res
+
+def entero_a_romano(n_int):
+    if n_int > 3999:
+        raise RomanNumberError("RomanNumber must be less of 4000")
+
+    digitos = listar_numero(n_int)
+
+    resultado = ""
+    for digito in digitos:
+        if digito == 0:
+            continue
+
+        clave, digito = sacar_clave(digito)
+        resultado += logica_aplastante(digito, clave)
+       
+    return resultado
+
+if __name__ == "__main__":
+    print(entero_a_romano(11))
+
+
 ''' 
 # Diccionario de Símbolos Romanos
 simbolos = {
@@ -102,10 +173,13 @@ print(devolver_total_romano(1000))
 print(devolver_total_romano(3999))
 print(devolver_total_romano(10000))
 print(devolver_total_romano(100000))
+print()
 print(devolver_total_romano(1000000))
-print(devolver_total_romano(10000000))
+print(devolver_total_romano(4000000))
+print(devolver_total_romano(3999999))
+print(devolver_total_romano(4000004040004000040000))
+
+#un millon es = (M)
 
 
-
-
-
+'''
--- test_romannumbers.py
+++ test_romannumbers.py
@@ -1,4 +1,5 @@
-from romannumbers import entero_a_romano
+from romannumbers import entero_a_romano, RomanNumberError
+import pytest
 
 def test_uno_es_palito():
     assert entero_a_romano(1) == "I"
@@ -29,5 +30,36 @@ def test_nueve_es_palito_y_equis():
 
 def test_diez_es_una_equis():
     assert entero_a_romano(10) == "X"
+
+def test_veinte_es_dos_equis_():
+    assert entero_a_romano(20) == "XX"
+
+def test_resto_de_decenas():
+    assert entero_a_romano(30) == "XXX"
+    assert entero_a_romano(40) == "XL"
+    assert entero_a_romano(50) == "L"
+    assert entero_a_romano(60) == "LX"
+    assert entero_a_romano(70) == "LXX"
+    assert entero_a_romano(80) == "LXXX"
+    assert entero_a_romano(90) == "XC"
+
+def test_once_es_equis_palito():
+    assert entero_a_romano(11) == "XI"
+    
+def test_101_es_C_palito():
+    assert entero_a_romano(101) == "CI"
+
+def test_cero_es_bada():
+    assert entero_a_romano(0) == ""
+    
+def test_numeros_rarunos():
+    assert entero_a_romano(2345) == "MMCCCXLV"
+    assert entero_a_romano(3999) == "MMMCMXCIX"
+
+def test_4000_es_error():
+    with pytest.raises(RomanNumberError):
+        entero_a_romano(4000)
+
+
 
 
